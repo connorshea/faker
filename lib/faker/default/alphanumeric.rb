34e0f1b8db1f4c545db2f29e6cf767555db9d614
Stephen A. Wilson <Stephen-356@hotmail.com>
2019-04-06 23:21:23 -0400

    add documentation to Alphanumeric

--- lib/faker/default/alphanumeric.rb
+++ lib/faker/default/alphanumeric.rb
@@ -3,9 +3,27 @@
 module Faker
   class Alphanumeric < Base
     class << self
+      ##
+      # List of characters allowed for alphabetic strings
+      # @private
       ALPHABET = ('a'..'z').to_a
+
+      ##
+      # List of characters allowed for alphanumeric strings
+      # @private
       ALPHANUMS = ALPHABET + (0..9).to_a
 
+      ##
+      # Produces a random string of alphabetic characters (no digits)
+      #
+      # @param char_count {Integer} The length of the string to generate
+      #
+      # @return {String}
+      #
+      # @example Faker::Alphanumeric.alpha(10)
+      #   "zlvubkrwga"
+      #
+      # @faker.version 1.9.2
       def alpha(number: 32)
         char_count = resolve(number)
         return '' if char_count.to_i < 1
@@ -13,6 +31,17 @@ module Faker
         Array.new(char_count) { sample(ALPHABET) }.join
       end
 
+      ##
+      # Produces a random string of alphanumeric characters
+      #
+      # @param char_count {Integer} The length of the string to generate
+      #
+      # @return {String}
+      #
+      # @example Faker::Alphanumeric.alpha(10)
+      #   "3yfq2phxtb"
+      #
+      # @faker.version 1.9.2
       def alphanumeric(number: 32)
         char_count = resolve(number)
         return '' if char_count.to_i < 1
